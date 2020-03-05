96b14971adb49467dbbbe5ad182dcb3d45bd6592
Supriyo Biswas <supriyo.sb@gmail.com>
2020-03-05 23:03:14 +0530

    data set added

--- cypress/integration/examples/CapsuleCRM.spec.js
+++ cypress/integration/examples/CapsuleCRM.spec.js
@@ -4,11 +4,17 @@ import LoginPage from '../pageObjects/LoginPage'
 
 describe('Capsule CRM spec 1', function(){
 
+    before(function(){
+        cy.fixture('user_data').then((user_data) => {
+            this.user_data = user_data
+        })
+    })
+
     it('Verify Login to Capsule CRM page', function(){
         const lp = new LoginPage()
         lp.visit()
         cy.title().should('be.equal', 'Home CRM')
-        lp.login('supriyo.sb', 'berhampore')
+        lp.login(this.user_data.user_name, this.user_data.password)
         cy.title().should('be.equal', 'Dashboard | Home CRM')
     })
 
